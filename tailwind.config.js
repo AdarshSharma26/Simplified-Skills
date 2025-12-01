d9d1610e2175ab1c43e99e2d163ddf277dbac58a
Adarsh Sharma <86520536+AdarshSharma26@users.noreply.github.com>
2025-12-01 15:35:50 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}" // Ensure all React files are scanned
+  ],
+  theme: {},
+  plugins: [],
+}
+
